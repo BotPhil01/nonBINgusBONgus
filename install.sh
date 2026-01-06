d72d3e127506272dfddb91d8019cfedef9d48d2e
ahx <acsrobles39@proton.me>
2026-01-06 16:01:19 +0000

    temp commit

--- install.sh
+++ install.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-bins="$(find . -maxdepth 1 -type f -executable)"
+bins="$(find bins/ -maxdepth 1 -type f -executable)"
 
 while IFS="" read -r line; do
-    if [[ "$line" == "./install.sh"]]; then
-        continue
-    fi
-    stripped="$(echo $line | sed -s 's/\.\///')"
-    sudo cp "bins/$line" /usr/bin/
-    sudo ln -sf "/usr/bin/$stripped" "$line" 
+    stripped="$(echo $line | sed -s 's/.*\///')"
+    sudo cp "bins/$stripped" /usr/bin/
+    sudo ln -sf "/usr/bin/$stripped" "./bin/$stripped" 
 done <<< "$bins"
