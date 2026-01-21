a4cfdc6f4683a7e4bafec1d914fbcad13995f5f8
BotPhil01 <110259538+BotPhil01@users.noreply.github.com>
2026-01-21 14:43:07 +0000

    Update pysim and safe install

--- install.sh
+++ install.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
-bins="$(find bins/ -maxdepth 1 -type f -executable)"
+bins="$(find bins/ -maxdepth 1 -type f -executable -print)"
 
-while IFS="" read -r line; do
-    stripped="$(echo $line | sed -s 's/.*\///')"
-    sudo cp "bins/$stripped" /usr/bin/
-done <<< "$bins"
+declare -a array=($bins)
+declare -i i=0
+for elem in "${array[@]}"; do
+    echo "$i $elem"
+    i+=1
+    sudo cp -i "$elem" /usr/bin/
+done
+# for elem in "${array[@]}"; do
+#     echo "$elem"
+# done
+# while IFS="" read -r line; do
+#     stripped="$(echo $line | sed -s 's/.*\///')"
+#     sudo cp -i "bins/$stripped" /usr/bin/
+# done <<< "$bins"
